a1e1140ae8ba0d36943e9a9d389e3ae94d75adc5
Pranay207 <lakshmipranaytej@gmail.com>
2025-11-05 10:26:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5000,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: [
+      "04083dbd-d36d-4f6d-a31b-60a2c71f49ea-00-3nxqn6ftxkbnl.kirk.replit.dev"
+    ]
   },
   resolve: {
     alias: {
